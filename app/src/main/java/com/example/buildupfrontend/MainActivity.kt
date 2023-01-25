0715c766166bc3b9946065208e272c2affa146b2
songfox00 <songfox00@gmail.com>
2023-01-25 23:06:00 +0900

    BottomNavigation 구현

--- app/src/main/java/com/example/buildupfrontend/MainActivity.kt
+++ app/src/main/java/com/example/buildupfrontend/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.buildupfrontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buildupfrontend.databinding.ActivityMainBinding
+import com.example.buildupfrontend.home.HomeFragment
+import com.example.buildupfrontend.mypage.MypageFragment
+import com.example.buildupfrontend.record.RecordFragment
+import com.example.buildupfrontend.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomnav.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.bottomnav_home -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.framelayout_main, HomeFragment()).commit()
+                    }
+                    R.id.bottomnav_record -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.framelayout_main, RecordFragment()).commit()
+                    }
+                    R.id.bottomnav_search -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.framelayout_main, SearchFragment()).commit()
+                    }
+                    R.id.bottomnav_mypage -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.framelayout_main, MypageFragment()).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.bottomnav_home
+            //첫번째 탭이 선택된 상태로 있도록
+        }
     }
 }
